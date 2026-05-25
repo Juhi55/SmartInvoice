1a11d944b02e81f9ecd949d9c07f54045ffc17d2
juhi55 <cjuhi888@gmail.com>
2026-05-25 15:55:07 +0530

    created customer model

---
+++ backend/models/Customer.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+    },
+
+    address: {
+      type: String,
+    },
+
+    gstNumber: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model(
+  "Customer",
+  customerSchema
+);
